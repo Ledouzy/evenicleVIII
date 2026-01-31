963e18e0d9f776c80135cacc44868195dfe08c1b
nericat2005 <nericat2005@gmail.com>
2026-01-31 17:43:56 -0500

    update
    attack implementation

--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -2,14 +2,13 @@ using System.Data;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections;
+using System.Security.Cryptography.X509Certificates;
 
 public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
 public class Battle_System : MonoBehaviour
 {
     public GameObject playerPrefab;
-
-
-
     public GameObject enemyPrefab;
 
 
@@ -22,26 +21,70 @@ public class Battle_System : MonoBehaviour
 
     public TextMeshProUGUI dialogueText; 
     //public TMPText dialogueText;
+
+    public BattleHUD playerHUD;
+    public BattleHUD enemyHUD;
+
     public BattleState state;
 
     void Start()
     {
         state = BattleState.START;
-        SetupBattle();
+        //Wrap coroutine for time delay
+        StartCoroutine(SetupBattle());
     }
 
-    void SetupBattle()
+    IEnumerator SetupBattle()
     {
         GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+        //NAME PLAYER UNDER PLAYER COMPONENTS
         playerUnit = playerGO.GetComponent<Unit>(); 
 
 
         GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
+        //NAME ENEMY UNDER ENEMY COMPONENTS
         enemyUnit = enemyGO.GetComponent<Unit>(); 
 
         
 
-        dialogueText.text = "text: enemy appeared!" + enemyUnit.unitName + "appraoches";
+        dialogueText.text = "text: enemy appeared!" + enemyUnit.unitName + "approaches";
+        playerHUD.SetHUD(playerUnit);
+        enemyHUD.SetHUD(enemyUnit);
+
+
+        //time delay
+        yield return new WaitForSeconds(2f);
+
+
+        //begins the turns
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
+        //state = BattleState.ENEMYTURN;
+        //EnemyTurn();
+    }
+
+    IEnumerator PlayerAttack()
+    {
+        yield return new WaitForSeconds(2f);
 
     }
+
+    void PlayerTurn()
+    {
+        dialogueText.text = "it is now the player's turn.";
+
+    }
+
+    public void OnAttackButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+        {
+            return;
+        }
+
+        StartCoroutine(PlayerAttack());
+    }
+
+
+
 }
