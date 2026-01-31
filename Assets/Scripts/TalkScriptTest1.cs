6c582adc09c7fcab7da1ec034e4e5b86a6688ee7
nericat2005 <nericat2005@gmail.com>
2026-01-31 11:59:00 -0500

    modify Talk
    Talk scene added stats bar and buttons

---
+++ Assets/Scripts/TalkScriptTest1.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TalkScriptTest1 : MonoBehaviour
+{
+    public void TalkScene1()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+}
