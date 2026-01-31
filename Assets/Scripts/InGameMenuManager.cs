3bdf405d465c787bf54a642d6b1379344d6a6c2d
Dialas-Illa <renaud.levac@gmail.com>
2026-01-31 17:34:20 -0500

    Fixed_code

--- Assets/Scripts/InGameMenuManager.cs
+++ Assets/Scripts/InGameMenuManager.cs
@@ -2,14 +2,74 @@ using UnityEngine;
 
 public class Inventory_Manager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public GameObject Inventory_Button;
+    public GameObject Item_Buttom;
+    public GameObject Status_Button;
+    public GameObject Equip_Button;
+    public GameObject Save_Button;
+    public GameObject Load_Button;
+    public GameObject Exit_Button;
+
+
+    void enableall()
+    {
+        Inventory_Button.SetActive(true);
+        Item_Buttom.SetActive(true);
+        Status_Button.SetActive(true);
+        Equip_Button.SetActive(true);
+        Save_Button.SetActive(true);
+        Load_Button.SetActive(true);
+        Exit_Button.SetActive(true);
+    }
+    void disableall()
+    {
+        Inventory_Button.SetActive(false);
+        Item_Buttom.SetActive(false);
+        Status_Button.SetActive(false);
+        Equip_Button.SetActive(false);
+        Save_Button.SetActive(false);
+        Load_Button.SetActive(false);
+        Exit_Button.SetActive(false);
+    }
+
+
     void Start()
+    {
+        enableall();
+    }
+
+    public void Inventory_Button_Pressed()
+    {
+        disableall();
+    }
+
+    public void Item_Buttom_Pressed()
+    {
+        disableall();
+    }
+
+    public void Status_Button_Pressed()
+    {
+        disableall();
+    }
+
+    public void Equip_Button_Pressed()
+    {
+        disableall();
+    }
+
+    public void Save_Button_Pressed()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Load_Button_Pressed()
+    {
+        
+    }
+
+    public void Exit_Button_Pressed()
     {
         
     }
