d291c50bdda4ea6decfe861ab5ce68e821d4967c
nericat2005 <nericat2005@gmail.com>
2026-01-31 15:04:05 -0500

    Problem persists
    Objects outside of Canvas UI children cannot be seen during gameplay.

--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -1,18 +1,32 @@
+using System.Data;
 using UnityEngine;
 
 public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
 public class Battle_System : MonoBehaviour
 {
     public GameObject playerPrefab;
-    public Transform playerBattleStation;
+
 
 
     public GameObject enemyPrefab;
+
+
+//MAYBE DELETE TRANSFORM??? NOT SURE
+    public Transform playerBattleStation;
     public Transform enemyBattleStation;
+
+    public BattleState state;
 
     void Start()
     {
-        
+        state = BattleState.START;
+        SetupBattle();
     }
 
+    void SetupBattle()
+    {
+        Instantiate(playerPrefab, playerBattleStation);
+        Instantiate(enemyPrefab, enemyBattleStation);
+
+    }
 }
