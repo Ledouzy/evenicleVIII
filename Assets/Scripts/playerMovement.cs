859b6fb40451daae63ae7d7e2bf94a4e8758ade2
Ledouzy <162760592+Ledouzy@users.noreply.github.com>
2026-01-31 21:44:56 -0500

    random encounter done
    it's in the player movement

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class playerMovement : MonoBehaviour
 {
     InputAction moveAction;
     Rigidbody2D Hunter;
+
     public float speed = 10f;
+    private int steps = 0;
+    [SerializeField] private int encounterRate=235;
     
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -13,7 +17,7 @@ public class playerMovement : MonoBehaviour
     {
         moveAction = InputSystem.actions.FindAction("Move");
         Hunter = GetComponent<Rigidbody2D>();
-     
+        InvokeRepeating(nameof(Encounter), 2.0f, 1f);
         
     }
 
@@ -21,8 +25,27 @@ public class playerMovement : MonoBehaviour
     void Update()
     {
         Vector2 movementVector = moveAction.ReadValue<Vector2>();
-        Hunter.linearVelocity = movementVector * speed;
-       
-        
+        Hunter.linearVelocity = movementVector * speed; 
+    }
+
+    void Encounter()
+    {
+        Vector2 movementVector = moveAction.ReadValue<Vector2>();
+        //Debug.Log(movementVector);
+        if (movementVector != new Vector2(0, 0))
+        {
+            steps++;
+            int randomNumber = Random.Range(steps,256);
+            
+            Debug.Log("steps: "+steps);
+            /*Debug.Log("random no:"+randomNumber);
+            Debug.Log("encounterRate: "+encounterRate);
+            */
+            if (randomNumber > encounterRate)
+            {
+                Debug.Log("Encounter!");
+                steps = 0;
+            }
+        }
     }
 }
