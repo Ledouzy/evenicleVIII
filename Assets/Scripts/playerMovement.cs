57a13879a241a0add0e449020f6da4546a16a174
Aziz Kitana <aziz.kitana@gmail.com>
2026-01-31 21:06:26 -0500

    basic map completed

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -5,13 +5,23 @@ public class playerMovement : MonoBehaviour
 {
     InputAction moveAction;
     Rigidbody2D Hunter;
+
     public float speed = 10f;
+     
+    enum CardinalDirection
+    {
+        EAST = 0,
+        NORTH = 90,
+        WEST = 180,
+        SOUTH = 270,
+    }
     
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
+      
         Hunter = GetComponent<Rigidbody2D>();
      
         
@@ -20,7 +30,9 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    
         Vector2 movementVector = moveAction.ReadValue<Vector2>();
+        
         Hunter.linearVelocity = movementVector * speed;
        
         
