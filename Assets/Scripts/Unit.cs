b8ccbd8a3f2cd3c64c8f73b2fdfaa13a996579ce
Dialas-Illa <renaud.levac@gmail.com>
2026-02-20 19:31:09 -0500

    Destroying body part
    Pretty simple code but it works

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,20 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
-    public string unitName;
-    public int unitLevel;
-
-    public int damage;
-    
-    public int maxHP;
-    public int currentHP;
+    public int max_HP, ATK, MATK, DEF, MDEF, SPD, ACC, AGI, LCK, DATE_HP, TYPE, LEVEL, exp, CAD, cur_HP, damage;
+    //I think the new is needed allocate memory.
+    public LinkedList<(int hardness, int attack)> appendage = new LinkedList<(int, int)>();
+    public string name;
 
     public bool TakeDamage(int dmg)
     {
-        currentHP-=dmg;
+        cur_HP-=dmg;
 
-        if (currentHP <= 0)
+        if (cur_HP <= 0)
         {
             return true;
         }   
